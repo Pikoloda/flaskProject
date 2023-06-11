6d54e9e23398424a272b6818304b68f2dc652a21
pikoloda <pikoloda@student.wszib.edu.pl>
2023-06-11 14:29:28 +0200

    Flask 11.06 - do poprawy

--- model/country.py
+++ model/country.py
@@ -1,4 +1,6 @@
 import json
+import pycountry_convert
+
 
 def load_db():
     with open('country_info.json', encoding='utf-8') as mock_database:
@@ -15,4 +17,25 @@ def find_by_index(index: int):
     return db[index]
 
 
+def find_continent_by_cc(country_code):
+    try:
+        continent_code = pycountry_convert.country_alpha2_to_continent_code(country_code)
+        continent_name = pycountry_convert.convert_continent_code_to_continent_name(continent_code)
+        counter = continent_map.get(continent_name,0)
+        continent_map[continent_name] = counter + 1
+        return continent_name
+    except KeyError:
+        return None
+
+
+
 db = load_db()
+
+continent_map = {}
+
+
+
+
+
+
+
